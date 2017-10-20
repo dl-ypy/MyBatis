4634d597fe7db00595632478dfacb79afbc2f01c
ypy <792015431@qq.com>
2017-10-20 16:53:36 +0800

    在mapper中添加ognl表达式

--- Mybatis_day01/src/com/ypy/model/UserModel.java
+++ Mybatis_day01/src/com/ypy/model/UserModel.java
@@ -1,9 +1,13 @@
 package com.ypy.model;
 
+import java.util.List;
+
 public class UserModel {
 	private int userid;
 	private String username;
 	private String password;
+	
+	private List<Integer> idList;   //in条件查询时用
 	@Override
 	public String toString() {
 		return "UserModel [userid=" + userid + ", username=" + username + ", password=" + password + "]";
@@ -25,5 +29,11 @@ public class UserModel {
 	}
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	public List<Integer> getIdList() {
+		return idList;
+	}
+	public void setIdList(List<Integer> idList) {
+		this.idList = idList;
 	}
 }
--- Mybatis_day01/src/com/ypy/test/UserTest.java
+++ Mybatis_day01/src/com/ypy/test/UserTest.java
@@ -2,22 +2,18 @@ package com.ypy.test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.junit.Before;
 import org.junit.Test;
 
 import com.ypy.model.UserModel;
 
 public class UserTest {
-
-	@Before
-	public void setUp() throws Exception {
-	}
 
 	@Test
 	public void testQuery() throws IOException {
@@ -27,7 +23,77 @@ public class UserTest {
 		SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(in);
 		//3.获取session
 		SqlSession session = factory.openSession();
-		List<UserModel> list = session.selectList("com.ypy.model.UserModel.query");
+		UserModel user = new UserModel();
+		user.setUsername("%改");
+		user.setPassword("111");
+		List<UserModel> list = session.selectList("com.ypy.model.UserModel.query", user);
+		for (UserModel userModel : list) {
+			System.out.println(userModel);
+		}
+		session.close();
+	}
+	
+	/**
+	 * 有优先级的查询
+	 * @throws IOException
+	 */
+	@Test
+	public void testQuery1() throws IOException {
+		//1.加载配置文件
+		InputStream in = Resources.getResourceAsStream("MyBatis-config.xml");
+		//2.获取SqlSessionFactory
+		SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(in);
+		//3.获取session
+		SqlSession session = factory.openSession();
+		UserModel user = new UserModel();
+		user.setUsername("%改");
+		user.setPassword("111");
+		List<UserModel> list = session.selectList("com.ypy.model.UserModel.query1", user);
+		for (UserModel userModel : list) {
+			System.out.println(userModel);
+		}
+		session.close();
+	}
+	
+	/**
+	 * in条件查询
+	 * @throws IOException
+	 */
+	@Test
+	public void testQuery2() throws IOException {
+		//1.加载配置文件
+		InputStream in = Resources.getResourceAsStream("MyBatis-config.xml");
+		//2.获取SqlSessionFactory
+		SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(in);
+		//3.获取session
+		SqlSession session = factory.openSession();
+		UserModel user = new UserModel();
+		List<Integer> idList = new ArrayList<Integer>();
+		idList.add(24);
+		idList.add(25);
+		idList.add(26);
+		user.setIdList(idList);
+		List<UserModel> list = session.selectList("com.ypy.model.UserModel.query2", user);
+		for (UserModel userModel : list) {
+			System.out.println(userModel);
+		}
+		session.close();
+	}
+	
+	/**
+	 * 查询别名，用resultMap
+	 * @throws IOException
+	 */
+	@Test
+	public void testQuery3() throws IOException {
+		//1.加载配置文件
+		InputStream in = Resources.getResourceAsStream("MyBatis-config.xml");
+		//2.获取SqlSessionFactory
+		SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(in);
+		//3.获取session
+		SqlSession session = factory.openSession();
+		UserModel user = new UserModel();
+		List<UserModel> list = session.selectList("com.ypy.model.UserModel.query3", user);
 		for (UserModel userModel : list) {
 			System.out.println(userModel);
 		}
@@ -65,8 +131,9 @@ public class UserTest {
 		} catch (Exception e) {
 			e.printStackTrace();
 			session.rollback();
+		} finally {
+			session.close();
 		}
-		session.close();
 	}
 	
 	@Test
@@ -78,7 +145,7 @@ public class UserTest {
 		//3.获取session
 		SqlSession session = factory.openSession();
 		UserModel user = new UserModel();
-		user.setUserid(24);
+		user.setUserid(25);
 		user.setUsername("修改");
 		user.setPassword("111");
 		//添加事务
@@ -88,8 +155,9 @@ public class UserTest {
 		} catch (Exception e) {
 			e.printStackTrace();
 			session.rollback();
+		} finally {
+			session.close();
 		}
-		session.close();
 	}
 	
 	@Test
@@ -107,8 +175,9 @@ public class UserTest {
 		} catch (Exception e) {
 			e.printStackTrace();
 			session.rollback();
+		} finally {
+			session.close();
 		}
-		session.close();
 	}
 
 }
